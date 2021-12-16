a51f6f8ae3645ad02c469b25e5e5b34846b6f9e7
ilodev <ilodev@users.noreply.github.com>
2021-12-16 10:43:03 -0800

    Added PreparedStatement shell commands
    More refactor and clean up

--- Source/Jurassic World Evolution 2/ACSE/Main/database.acseluadatabase.lua
+++ Source/Jurassic World Evolution 2/ACSE/Main/database.acseluadatabase.lua
@@ -11,7 +11,7 @@ local api = global.api
 local table = global.table
 local pairs = global.pairs
 local ipairs = global.ipairs
-local type  = global.type
+local type = global.type
 local require = require
 local ACSEDatabase = module(...)
 local Vector3 = require("Vector3")
@@ -26,47 +26,49 @@ global.api.acse.GetACSEVersionString = function()
     return global.tostring(global.api.acse.versionNumber)
 end
 
+
+global.api.acsedebug = {}
+
 -- @brief logging/tracing functions
-global.api.acse.Trace = function(msg)
+global.api.acsedebug.Trace = function(msg)
     global.loadfile("acse :" .. msg)
 end
-global.api.acse.WriteLine = function(channel, msg)
-    local tChannel = {'output', 'error', 'unknown'}
+global.api.acsedebug.WriteLine = function(channel, msg)
+    local tChannel = {"output", "error", "unknown"}
     global.api.debug.Trace(msg)
 end
-global.api.acse.Print = function(msg, color)
+global.api.acsedebug.Print = function(msg, color)
     global.api.debug.Trace(msg)
 end
-global.api.acse.Error = function(msg)
+global.api.acsedebug.Error = function(msg)
     global.api.debug.Trace("-Err- " .. msg)
 end
-global.api.acse.Warning = function(msg)
+global.api.acsedebug.Warning = function(msg)
     global.api.debug.Trace("-Wrn- " .. msg)
 end
-global.api.acse.Assert = function(cond, msg)
-    if cond == false then 
-        global.api.debug.Trace("-Assert- " .. global.tostring(msg) )
+global.api.acsedebug.Assert = function(cond, msg)
+    if cond == false then
+        global.api.debug.Trace("-Assert- " .. global.tostring(msg))
     end
     return cond
 end
 
-
 -- Tweakable support
-global.api.acse.tTweakables = {}
+global.api.acsedebug.tTweakables = {}
 
-global.api.acse.GetTweakables = function()
-    return global.api.acse.tTweakables
+global.api.acsedebug.GetTweakables = function()
+    return global.api.acsedebug.tTweakables
 end
 
 --/@brief make our own tweakables manager
-global.api.acse.CreateDebugTweakable = function(ttype, id, arg1, arg2, arg3, arg4)
+global.api.acsedebug.CreateDebugTweakable = function(ttype, id, arg1, arg2, arg3, arg4)
     --/ Tweakable types: 22 boolean, 11 float, 8 integer64, 7 integer32
     --/ tweakable exists, return the original one
 
     local nid = global.string.lower(id)
 
-    if global.api.acse.tTweakables[nid] then
-        return global.api.acse.tTweakables[nid]
+    if global.api.acsedebug.tTweakables[nid] then
+        return global.api.acsedebug.tTweakables[nid]
     end
 
     --/ make a new tweakable
@@ -87,31 +89,31 @@ global.api.acse.CreateDebugTweakable = function(ttype, id, arg1, arg2, arg3, arg
     end
 
     --/ save the tweakable
-    global.api.acse.tTweakables[nid] = tweakable
+    global.api.acsedebug.tTweakables[nid] = tweakable
     return tweakable
 end
 
 --/@brief retrieve a tweakable object from the list if exists.
-global.api.acse.GetDebugTweakable = function(id)
+global.api.acsedebug.GetDebugTweakable = function(id)
     local nid = global.string.lower(id)
 
-    if global.api.acse.tTweakables[nid] then
-        return global.api.acse.tTweakables[nid]
+    if global.api.acsedebug.tTweakables[nid] then
+        return global.api.acsedebug.tTweakables[nid]
     end
 
     return nil
 end
 
 -- // Shell commands support
-global.api.acse.tShellCommands = {}
+global.api.acsedebug.tShellCommands = {}
 
 -- @brief splits a string by ' ' character
 function Split(s, delimiter)
-    result = {};
-    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
-        table.insert(result, match);
+    result = {}
+    for match in (s .. delimiter):gmatch("(.-)" .. delimiter) do
+        table.insert(result, match)
     end
-    return result;
+    return result
 end
 
 -- @splits a string by spaces, respecting quoted strings
@@ -119,20 +121,24 @@ function stringSplit(text)
     local tout = {}
     local e = 0
     while true do
-        local b = e+1
-        b = text:find("%S",b)
-        if b==nil then break end
-        if text:sub(b,b)=="'" then
-            e = text:find("'",b+1)
-            b = b+1
-        elseif text:sub(b,b)=='"' then
-            e = text:find('"',b+1)
-            b = b+1
-        else
-            e = text:find("%s",b+1)
+        local b = e + 1
+        b = text:find("%S", b)
+        if b == nil then
+            break
         end
-        if e==nil then e=#text+1 end
-        global.table.insert(tout, text:sub(b,e-1))
+        if text:sub(b, b) == "'" then
+            e = text:find("'", b + 1)
+            b = b + 1
+        elseif text:sub(b, b) == '"' then
+            e = text:find('"', b + 1)
+            b = b + 1
+        else
+            e = text:find("%s", b + 1)
+        end
+        if e == nil then
+            e = #text + 1
+        end
+        global.table.insert(tout, text:sub(b, e - 1))
     end
     return tout
 end
@@ -140,79 +146,86 @@ end
 -- @brief from &Time&Of&Day command string, returns tod
 function getCommandShortName(text)
     local tout = {}
-    for match in sStr:gmatch( "&(.)") do
-        table.insert(tout, match:lower());
+    for match in sStr:gmatch("&(.)") do
+        table.insert(tout, match:lower())
     end
     return global.table.concat(tout)
 end
 
-
 -- @brief adds a command to the list
-global.api.acse.RegisterShellCommand = function(_fn, sCmd, sDesc)
+global.api.acsedebug.RegisterShellCommand = function(_fn, sCmd, sDesc)
     --/ make a new command
     local command = {}
     command._fn = _fn
-    command._sCmd = global.string.gsub(sCmd,'&','') -- Remove case sensitivity marker
+    command._sCmd = global.string.gsub(sCmd, "&", "") -- Remove case sensitivity marker
     command._sDesc = sDesc
 
     name = stringSplit(command._sCmd)[1]
     name = global.string.lower(name)
 
     --/ save the tweakable
-    global.api.acse.tShellCommands[name] = command
-    return global.api.acse.tShellCommands[name]
+    global.api.acsedebug.tShellCommands[name] = command
+    return global.api.acsedebug.tShellCommands[name]
 end
 
 -- @brief Removes a command from the list
-global.api.acse.UnregisterShellCommand = function(tCmd)
+global.api.acsedebug.UnregisterShellCommand = function(tCmd)
     name = stringSplit(tCmd._sCmd)[1]
     name = global.string.lower(name)
-    global.api.acse.tShellCommands[name] = nil
+    global.api.acsedebug.tShellCommands[name] = nil
     return
 end
 
 -- @brief Runs a command
-global.api.acse.RunShellCommand = function(sCmd)
-
+global.api.acsedebug.RunShellCommand = function(sCmd)
     -- this RunShellCommand will fail until we handle missing argument types (vector:3 etc..)
-	tArgs = stringSplit(sCmd)
-	name = tArgs[1]
+    tArgs = stringSplit(sCmd)
+    name = tArgs[1]
     name = global.string.lower(name)
-	global.table.remove(tArgs, 1)
+    global.table.remove(tArgs, 1)
 
-    local cmd = global.api.acse.tShellCommands[name]
+    local cmd = global.api.acsedebug.tShellCommands[name]
     if cmd ~= nil then
-
-	    -- Convert Arg types
-	   	tCArgs = stringSplit(cmd._sCmd)
-	   	global.table.remove(tCArgs, 1)
+        -- Convert Arg types
+        tCArgs = stringSplit(cmd._sCmd)
+        global.table.remove(tCArgs, 1)
 
         for i, v in global.pairs(tArgs) do
             if tCArgs[i] ~= nil then
-                if global.string.match(tCArgs[i],"{string}") then end -- do nothing, for SetTweakable
-                if global.string.match(tCArgs[i],"{value}") then end -- do nothing, for SetTweakable
-                if global.string.match(tCArgs[i],"{float}") then tArgs[i] = global.tonumber(tArgs[i]) end
-                if global.string.match(tCArgs[i],"{int32}") then tArgs[i] = global.tonumber(tArgs[i]) end
-                if global.string.match(tCArgs[i],"{uint32}") then tArgs[i] = global.tonumber(tArgs[i]) end
-                if global.string.match(tCArgs[i],"{uint64}") then tArgs[i] = global.tonumber(tArgs[i]) end
+                if global.string.match(tCArgs[i], "{string}") then
+                end -- do nothing, for SetTweakable
+                if global.string.match(tCArgs[i], "{value}") then
+                end -- do nothing, for SetTweakable
+                if global.string.match(tCArgs[i], "{float}") then
+                    tArgs[i] = global.tonumber(tArgs[i])
+                end
+                if global.string.match(tCArgs[i], "{int32}") then
+                    tArgs[i] = global.tonumber(tArgs[i])
+                end
+                if global.string.match(tCArgs[i], "{uint32}") then
+                    tArgs[i] = global.tonumber(tArgs[i])
+                end
+                if global.string.match(tCArgs[i], "{uint64}") then
+                    tArgs[i] = global.tonumber(tArgs[i])
+                end
                 -- Missing {notificationtype} {notificationcontexttype} {vector:3} and {vector:2}
                 -- also missing optional args []
-                local stringtoboolean={ ["true"]=true, ["false"]=false }
-                if global.string.match(tCArgs[i],"{boolean}") then tArgs[i] = stringtoboolean[tArgs[i]] end
+                local stringtoboolean = {["true"] = true, ["false"] = false}
+                if global.string.match(tCArgs[i], "{bool}") then
+                    tArgs[i] = stringtoboolean[tArgs[i]]
+                end
             end
         end
 
-        -- Some commands will try to print/dump into the environment.output stream, it just needs 
+        -- Some commands will try to print/dump into the environment.output stream, it just needs
         -- to be initialised for the game not crash
-        local tEnv  = api.game.GetEnvironment()
+        local tEnv = api.game.GetEnvironment()
         tEnv.output = 1
-        tEnv.error  = 2
+        tEnv.error = 2
 
-        local ret   = cmd._fn( api.game.GetEnvironment(), tArgs) -- Add args
+        local ret = cmd._fn(api.game.GetEnvironment(), tArgs) -- Add args
     end
 end
-
-
 
 global.api.acseentity = {}
 global.api.acseentity.rawFindPrefab = global.api.entity.FindPrefab
@@ -221,51 +234,37 @@ global.api.acseentity.rawInstantiatePrefab = global.api.entity.InstantiatePrefab
 global.api.acseentity.rawAddComponentsToEntity = global.api.entity.AddComponentsToEntity
 
 global.api.acseentity.FindPrefab = function(sPrefab)
-    global.api.debug.Trace("*** entity.FindPrefab func called with " .. sPrefab)
     return global.api.acseentity.rawFindPrefab(sPrefab)
 end
 global.api.acseentity.CompilePrefab = function(tPrefab, sPrefab)
-    global.api.debug.Trace("*** entity.CompilePrefab func called with " .. sPrefab)
+    -- global.api.debug.Trace("*** entity.CompilePrefab func called with " .. sPrefab)
     return global.api.acseentity.rawCompilePrefab(tPrefab, sPrefab)
 end
-global.api.acseentity.InstantiatePrefab = function(sPrefab, arg1, arg2, arg3, arg4, arg5, arg6)
+global.api.acseentity.InstantiatePrefab = function(sPrefab, ...)
 
+    --/ Physics world is the first prefab being instantiated in any game, 
+    --/ at this moment the entity component is ready so we will rebuild 
+    --/ the rest of prefabs defined by other mods. This piece in particular
+    --/ will come handy for prefabs required early in the loading process.
     if sPrefab == "PhysicsWorld" then
         local GameDatabase = require("Database.GameDatabase")
         if GameDatabase.GetLuaPrefabs then
-            for _sName, _tParams in pairs( GameDatabase.GetLuaPrefabs() ) do
-                api.debug.Trace("ACSE compiling prefab: " .. global.tostring(_sName))
-                local cPrefab = global.api.entity.CompilePrefab(_tParams, _sName)
-                if cPrefab == nil then
-                    api.debug.Trace("ACSE error compiling prefab: " .. _sName)
-                end
-            end
+            GameDatabase.BuildLuaPrefabs()
         end
     end
 
-
-    local entityId = global.api.acseentity.rawInstantiatePrefab(sPrefab, arg1, arg2, arg3, arg4, arg5, arg6)
-    global.api.debug.Trace("InstantitePrefab() of " .. sPrefab .. " with entityId : " .. entityId)
+    local entityId = global.api.acseentity.rawInstantiatePrefab(sPrefab, ...)
+    global.api.debug.Trace("Entity.InstantitePrefab() of " .. sPrefab .. " with entityId : " .. entityId)
     return entityId
 end
 global.api.acseentity.AddComponentsToEntity = function(nEntityId, tComponents)
     local ret = global.api.acseentity.rawAddComponentsToEntity(nEntityId, tComponents)
     global.api.debug.Trace(
-        "*** entity.AddComponentsToEntity " ..
-            type(nEntityId) .. " " .. type(tComponents) .. " " .. type(ret) .. " "
+        "Entity.AddComponentsToEntity() " .. type(nEntityId) .. " " .. type(tComponents) .. " " .. type(ret) .. " "
     )
 end
 
-global.api.entity = global.setmetatable(global.api.acseentity, {__index = global.api.entity})
-global.api.debug.Trace("+ api.entity patched")
-
-
-
-
-
-
 -- @brief add our custom databases
 ACSEDatabase.AddContentToCall = function(_tContentToCall)
-	table.insert(_tContentToCall, require("Database.ACSE"))
+    table.insert(_tContentToCall, require("Database.ACSE"))
 end
-
--- Source/Jurassic World Evolution 2/ACSE/Main/managers.acseparkmanager.lua
+++ Source/Jurassic World Evolution 2/ACSE/Main/managers.acseparkmanager.lua
@@ -24,25 +24,25 @@ ACSEParkManager.bInitOnSerialization = false
 
 -- @Brief Init function for this manager
 ACSEParkManager.Init = function(self, _tProperties, _tEnvironment)
-	api.debug.Trace("ACSEParkManager:Init()")
-
+    api.debug.Trace("ACSEParkManager:Init()")
     local tWorldAPIs = ((api.world).GetWorldAPIs)()
     self.worldSerialisationAPI = tWorldAPIs.worldserialisation
 
     -- Our serialization client would store component and other related ACSE provided
-    -- information. For now just creates the entry in the world serialization so we 
+    -- information. For now just creates the entry in the world serialization so we
     -- can initialize during loading.
     local fnSave = function(_tSave, _tParams)
-	  if _tParams.SerialisationType ~= (self.worldSerialisationAPI).Type_Island then
-	    return 
-	  end
-	  _tSave.tComponents = {}
-      return
+        if _tParams.SerialisationType ~= (self.worldSerialisationAPI).Type_Island then
+            -- All island type, better ignore this one
+            return
+        end
+        _tSave.tComponents = {}
+        return
     end
 
     -- Implementing prefab compilation during de-serialization.
     local fnLoad = function(_tLoad, _nLoadedVersion, _tParams)
-        -- We need to make any required prefab available before the island is loading and 
+        -- We need to make any required prefab available before the island is loading and
         -- other managers start instancing their entities.
         if ACSEParkManager.bInitOnSerialization == false then
             self:CompilePrefabs()
@@ -52,12 +52,12 @@ ACSEParkManager.Init = function(self, _tProperties, _tEnvironment)
     end
 
     local fnCanLoadOldVersion = function(_nOldVersion)
-      return true
+        return true
     end
 
+    --/ Register our world serialization clients
     self.worldSerialisationAPI:RegisterWorldSerialisationClient("ACSE", 1, fnSave, fnLoad, fnCanLoadOldVersion)
-
-
+    self.bInitOnSerialization = false
 end
 
 -- @Brief Update function for this manager
@@ -66,7 +66,7 @@ end
 
 -- @Brief Activate function for this manager
 ACSEParkManager.Activate = function(self)
-  --/ Initialize prefabs if they haven't been intialized during de-serialization
+    --/ Initialize prefabs if they haven't been intialized during de-serialization
     if ACSEParkManager.bInitOnSerialization == false then
         ACSEParkManager.bInitOnSerialization = true
         self:CompilePrefabs()
@@ -79,22 +79,17 @@ end
 
 -- @Brief Shutdown function for this manager
 ACSEParkManager.Shutdown = function(self)
-	api.debug.Trace("ACSEParkManager:Shutdown()")
+    api.debug.Trace("ACSEParkManager:Shutdown()")
     self.worldSerialisationAPI:UnregisterWorldSerialisationClient("ACSE")
+    self.bInitOnSerialization = false
 end
 
 -- @Brief Perform Lua compilation of prefabs
 ACSEParkManager.CompilePrefabs = function(self)
     api.debug.Trace("ACSEParkManager:CompilePrefabs()")
-    -- Merge default Managers with ACSE collected protos
+    -- Compile all prefabs added to the ACSE database.
     if GameDatabase.GetLuaPrefabs then
-        for _sName, _tParams in pairs( GameDatabase.GetLuaPrefabs() ) do
-            api.debug.Trace("ACSE compiling prefab: " .. global.tostring(_sName))
-            local cPrefab = global.api.entity.CompilePrefab(_tParams, _sName)
-            if cPrefab == nil then
-                api.debug.Trace("ACSE error compiling prefab: " .. _sName)
-            end
-        end
+        GameDatabase.BuildLuaPrefabs()
     end
 end
 
